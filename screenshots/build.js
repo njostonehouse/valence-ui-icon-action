28d432fe806273ddf982bb0ccd962651e52ff204
Noel Stonehouse <Noel.Stonehouse@D2L.com>
2016-04-01 15:22:03 -0400

    US68371: Create a way to take the docs snapshot. Update WC to work properly in phantomjs and Firefox.

--- screenshots/build.js
+++ screenshots/build.js
@@ -3,19 +3,21 @@ page.viewportSize = {width: 750, height: 1000};
 
 function capture(url, name, callback) {
 	page.open(url, function() {
-		var clipRect = page.evaluate(function (n) {
-			return document.querySelector('.screenshot-' + n)
-				.getBoundingClientRect();
-		}, name);
+		setTimeout(function() {
+			var clipRect = page.evaluate(function (n) {
+				return document.querySelector('.screenshot-' + n)
+					.getBoundingClientRect();
+			}, name);
 
-		page.clipRect = {
-			top: clipRect.top,
-			left: clipRect.left,
-			width: clipRect.width,
-			height: clipRect.height
-		};
-		page.render('./screenshots/' + name + '.png');
-		callback();
+			page.clipRect = {
+				top: clipRect.top,
+				left: clipRect.left,
+				width: clipRect.width,
+				height: clipRect.height
+			};
+			page.render('./screenshots/' + name + '.png');
+			callback();
+		}, 500);
 	});
 }
 
